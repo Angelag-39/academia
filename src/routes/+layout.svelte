fc2a8bfbbeec5503f7d0e4d36cb3f7d42f566e2b
Don David <angelaguacaran@gmail.com>
2024-03-22 15:21:58 -0300

    inicio da codificacao

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script>
 	import '../global.css'
 	import '@fontsource-variable/roboto-condensed';
-	import '@fontsource-variable/roboto-condensed/700.css';
-	import '@fontsource-variable/cabin';
+	/*import "@fontsource-variable/roboto-condensed/700.css";*/
+	import "@fontsource-variable/cabin";
 </script>
 
 <slot/>
-<h1>hola mundo </h1>
