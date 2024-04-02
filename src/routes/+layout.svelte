e5af8aad386a8918feaaa3e139e53ee96e216385
Don David <angelaguacaran@gmail.com>
2024-04-01 22:25:30 -0300

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,11 @@
     import "@fontsource-variable/roboto-condensed";
     import "@fontsource-variable/cabin"
     import Header from '$lib/Header.svelte'
+    import Footer from '$lib/Footer.svelte';
 </script>
 
 
 <Header/>
 <slot/>
-
+<Footer/>
 
